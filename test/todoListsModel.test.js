8c841f5c94bb414a79dd301b9687b1b24bb1bbfe
KarlOlsson90 <karl.olsson.mail@gmail.com>
2020-09-04 13:16:12 +0200

    WIP computer swap

--- test/todoListsModel.test.js
+++ test/todoListsModel.test.js
@@ -5,14 +5,15 @@ const {expect, request} = chai
 const app = require('../app')
 const {todoListsCollection: db} = require('../database/database');
 const model = require('../models/usersModel')
+
 const testobjektet = {
+    id: "",
     token: ""
 }
 
 describe('Create Todo List', () => { 
     
     beforeEach(async function(){
-        console.log("beforeEach körs")
         
         const testUser = await model.createUserModel({
 
@@ -22,13 +23,22 @@ describe('Create Todo List', () => {
         })
 
         testobjektet.token = await model.loginUserModel({email: testUser.email, password: '123'})
-
+        testobjektet.id = testUser._id
     });
 
     it('should create the list', function() {
-        const input = {listName: 'MyFirstList'}
-
+        const input = {listName: 'MyFirstList', userID: testobjektet.id}
         
+        expect(input.userID).to.be.string
+        expect(input.userID.length).to.be.equal(16)
+        
+        request(app)
+            .post('/todoLists')
+            .send(input)
+            .end((err,res) => {
+                expect(res).to.have.status(404)
+                })
+                
 
     });
 
