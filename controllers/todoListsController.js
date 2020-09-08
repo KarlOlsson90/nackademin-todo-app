c4f87baaec45d548ebad6c2d5b36612d5a8ba6af
KarlOlsson90 <karl.olsson.mail@gmail.com>
2020-09-08 13:27:16 +0200

    added routes and controller for todo lists

--- controllers/todoListsController.js
+++ controllers/todoListsController.js
@@ -1,5 +1,22 @@
+const model = require('../models/todoListsModel');
 
-
+async function getAllTodoListsController(req, res){
+    try {
+        var result = await model.getAllTodoListsModel()
+        return res.status(200).json(result);
+    } catch(error) {
+        return res.status(400).json(error);
+    }
+}
+async function getSingleTodoListController(req, res){
+    try {
+        var id = req.params.id
+        var result = await model.getSingleTodoListModel(id)
+        return res.status(200).json(result);
+    } catch(error) {
+        return res.status(400).json(error);
+        }
+}
 async function createTodoListController(req, res){
     try {
         const body = {title: req.body.title}
@@ -9,7 +26,30 @@ async function createTodoListController(req, res){
         return res.status(400).json(error);
     }
 }
+async function editTodoListController(req, res){
+    try {
+        var id = req.params.id
+        const body = {title: req.body.title, content: req.body.content, deadline: req.body.deadline}
+        var result = await model.editTodoListModel(id, body)
+        return res.status(204).json(result);
+    } catch(error) {
+        return res.status(400).json(error);
+    }
+}
+async function deleteTodoListController(req, res){
+    try {
+        const id = req.params.id
+        var result = await model.removeTodoListModel(id)
+        return res.status(204).json(result);
+    } catch(error) {
+        return res.status(400).json(error);
+    }
+}
 
 module.exports = {
-    createTodoListController
+    getAllTodoListsController,
+    getSingleTodoListController,
+    createTodoListController,
+    editTodoListController,
+    deleteTodoListController
 }
--- routes/todoListsRoute.js
+++ routes/todoListsRoute.js
@@ -6,6 +6,10 @@ const router = express.Router();
 
 const { authenticate } = require("../middlewares/authenticateMiddleware");
 
+//router.get('/', controller.)
+//router.get('/:id', controller.)
 router.post('/', authenticate, controller.createTodoListController)
+//router.patch('/:id', authenticate, controller.)
+//router.delete('/:id', authenticate, controller.)
 
 module.exports = router
