721b117166ee0f1d26605db518d2ba6bd8f2fa08
KarlOlsson90 <karl.olsson.mail@gmail.com>
2020-09-11 14:33:27 +0200

    added working endpoint for getting all user data

--- models/complianceModel.js
+++ models/complianceModel.js
@@ -1,11 +1,23 @@
 const {usersCollection: users_DB, todoListsCollection: todoLists_DB, todosCollection: todos_DB} = require('../database/database');
 
-async function getAllUserDataModel () {
-    console.log("hej")
+async function getAllUserDataModel (id) {
+    
+    var response = [];
+    userObject = await users_DB.findOne({ _id: id })
+    delete userObject.password
+    response.push(userObject)
+    
+    todoObject = await todos_DB.find({createdBy: id})
+    response.push(todoObject)
+
+    todoListObject = await todoLists_DB.find({createdBy: id})
+    response.push(todoListObject)
+    
+    return response;
 }
 
-async function clearUserDataModel () {
-    console.log("hå")
+async function clearUserDataModel (id) {
+    console.log("hej")
 }
 
 module.exports = {
