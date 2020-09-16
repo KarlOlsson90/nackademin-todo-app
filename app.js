56964452450e79c2ec1592feec3eca3bb4c5b70b
KarlOlsson90 <karl.olsson.mail@gmail.com>
2020-09-16 13:58:59 +0200

    changed port

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/', routes)
 
 //Server
-const port = 3000
+const port = 5000
 app.listen(port);
 console.log("Server running on port " + port + ";")
 
