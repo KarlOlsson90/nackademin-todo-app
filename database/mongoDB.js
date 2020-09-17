6edde660e811216237e1b9b9bbf5695fa2373c85
KarlOlsson90 <karl.olsson.mail@gmail.com>
2020-09-17 16:32:23 +0200

    Repaired mongoDB connection

--- database/mongoDB.js
+++ database/mongoDB.js
@@ -2,10 +2,12 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 
 let mongoDatabase
+
 switch(process.env.ENV){
     case 'test':
     const {MongoMemoryServer} = require('mongodb-memory-server')
-    mongoDatabase = new MongoMemoryServer()
+    mongoDatabase = new MongoMemoryServer({binary: {version: "4.4.1"}})
+    break;
     case 'dev':
         mongoDatabase = {
             getUri: async () => 
@@ -34,10 +36,14 @@ async function connect(){
 
 async function disconnect(){
     console.log("disconnect")
-    if(process.env.ENVIRONMENT == 'test' || process.env.ENVIRONMENT == 'development'){
-        await mongoDatabase.stop()
-    }
+
     await mongoose.disconnect()
+
+        if(process.env.ENV == 'test' || process.env.ENV == 'dev'){
+            await mongoDatabase.stop()
+        }
+
+
 }
 
 module.exports = {
